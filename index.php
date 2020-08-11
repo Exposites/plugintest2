4d550ec86d2f275989a7d847cf71cd2d9371612a
Jim Hunter <jim.hunter@corp.updatepromise.com>
2020-08-11 01:50:58 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Test Plugin
+ * Plugin URI:  https://
+ * Description: Basic plugin test 
+ * Version:     0.0.5
+ * Author:      Exposites.com
+ * Author URI:  https://
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: expotest
+ * Domain Path: /languages
+ * Plugin Type: Piklist
+ */
